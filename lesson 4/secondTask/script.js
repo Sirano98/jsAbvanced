1c3682dd1fec2e193fda6d1a5b278faae17c5922
Sirano98 <protsiuk1998@gmail.com>
2020-09-28 10:02:44 +0300

    no comments

--- lesson 4/secondTask/script.js
+++ lesson 4/secondTask/script.js
@@ -1,14 +1,19 @@
+window.onbeforeunload = function () {
+    return true;
+};
+
 var divElements = document.getElementsByTagName("div");
 
 function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-function getColor() {
+function getColor(event) {
     var color = "rgb(" + getRandomInRange(0, 255) + ", " + getRandomInRange(0, 255) + ", " + getRandomInRange(0, 255) + ")";
-    this.style.backgroundColor = color;
+    event.target.style.backgroundColor = color;
+    event.target.innerHTML = color;
 };
 
 for (var i = 0; i < divElements.length; i++) {
-    divElements[i].onclick = getColor;
+    divElements[i].onmouseover = getColor;
 };
